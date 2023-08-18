a6ed0e112cb4bfaddf69d61b75a5263cdb67c01d
Rajat U Krishna <rajatkrishna99@gmail.com>
2023-08-18 17:31:47 -0400

    Add OpenVINO support for RoBerta and XlmRoBerta embeddings

--- src/main/scala/com/johnsnowlabs/ml/ai/Bert.scala
+++ src/main/scala/com/johnsnowlabs/ml/ai/Bert.scala
@@ -135,12 +135,11 @@ private[johnsnowlabs] class Bert(
           } finally if (results != null) results.close()
         }
       case Openvino.name =>
-        val shape = Array(batchLength, maxSentenceLength)
-        val tokenTensors = new Tensor(shape, batch.flatMap(_.toSeq).toArray)
-        val segmentTensors = new Tensor(shape, Array.fill(batchLength * maxSentenceLength)(0))
-        val maskTensors = new Tensor(
-          shape,
-          batch.flatMap(sentence => sentence.map(x => if (x == 0) 0 else 1)).toArray)
+        val (tokenTensors, maskTensors, segmentTensors) =
+          PrepareEmbeddings.prepareOvIntBatchTensorsWithSegment(
+            batch = batch,
+            maxSentenceLength = maxSentenceLength,
+            batchLength = batchLength)
 
         val inferRequest = openvinoWrapper.get.getCompiledModel().create_infer_request()
         inferRequest.set_tensor(
@@ -270,12 +269,11 @@ private[johnsnowlabs] class Bert(
           } finally if (results != null) results.close()
         }
       case Openvino.name =>
-        val shape = Array(batchLength, maxSentenceLength)
-        val tokenTensors = new Tensor(shape, batch.flatMap(_.toSeq).toArray)
-        val segmentTensors = new Tensor(shape, Array.fill(batchLength * maxSentenceLength)(0))
-        val maskTensors = new Tensor(
-          shape,
-          batch.flatMap(sentence => sentence.map(x => if (x == 0) 0 else 1)).toArray)
+        val (tokenTensors, maskTensors, segmentTensors) =
+          PrepareEmbeddings.prepareOvIntBatchTensorsWithSegment(
+            batch = batch,
+            maxSentenceLength = maxSentenceLength,
+            batchLength = batchLength)
 
         val inferRequest = openvinoWrapper.get.getCompiledModel().create_infer_request
         inferRequest.set_tensor(
